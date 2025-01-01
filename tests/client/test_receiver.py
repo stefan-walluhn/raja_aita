e10b1b5a476a87efce0aaa3e95846f4add5ee5d1
Stefan Walluhn <stefan@walluhn.de>
2025-01-01 16:56:35 +0100

    fix tests

--- tests/client/test_receiver.py
+++ tests/client/test_receiver.py
@@ -1,6 +1,7 @@
 import pytest
 from datetime import datetime, timezone
 from unittest.mock import Mock
+from uuid import uuid4
 
 from raja_aita.client.gateway import Gateway
 from raja_aita.client.receiver import DBusSignalReceiver, TimerReceiver
@@ -26,7 +27,7 @@ class TestDBusSignalReceiver:
         receiver(None, {"IdleHint": False, "IdleSinceHint": 1735741149885606})
 
         state_manager.wake_up.assert_called_once_with(
-            datetime(2025, 1, 1, 14, 19, 9, tzinfo=timezone.utc)
+            datetime(2025, 1, 1, 14, 19, 9, 885606, tzinfo=timezone.utc)
         )
         state_manager.sleep.assert_not_called()
 
@@ -38,21 +39,26 @@ class TestDBusSignalReceiver:
 
 
 class TestTimerReciver:
-    def test_call_when_is_awake(self):
-        state = State(is_awake=True, awake_since=datetime.now().astimezone())
-        gateway = Mock(spec=Gateway)
+    @pytest.fixture
+    def gateway(self):
+        return Mock(spec=Gateway)
 
-        receiver = TimerReceiver(state, gateway)
+    @pytest.fixture
+    def state(self):
+        return State(is_awake=True, awake_since=datetime.now(tz=timezone.utc))
 
+    @pytest.fixture
+    def receiver(self, state, gateway):
+        return TimerReceiver(uuid4(), state, gateway)
+
+    def test_call_when_is_awake(self, gateway, receiver):
         receiver()
 
+        # XXX assert_called_once_with
         gateway.patch_beacon.assert_called_once()
 
-    def test_call_when_is_not_awake(self):
-        state = State(is_awake=False, awake_since=datetime.now().astimezone())
-        gateway = Mock(spec=Gateway)
-
-        receiver = TimerReceiver(state, gateway)
+    def test_call_when_is_not_awake(self, state, gateway, receiver):
+        state.is_awake = False
 
         receiver()
 
